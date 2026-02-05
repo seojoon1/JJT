2c75e785d630dd293d627eb660bcc150e60c48d6
comppy <kim1122aass@gmail.com>
2026-02-05 20:34:40 +0900

    feat/header

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,17 @@
-
-
+import Header from "./components/header"
+import { ThemeProvider } from "./contexts/ThemeContext"
+import { Routes, Route } from "react-router";
 function App() {
 
   return (
-    <div className='App'>
-      <header className='bg-white shadow-md'>
-        <nav className="container mx-auto px-6 py-4 flex justify-between items-center">
-          <div className="flex justify-between items-center"> 
-              <a href="/" className="text-2xl font-bold bg-gradient-to-r from-blue-600 to-indigo-600 bg-clip-text text-transparent">
-                seojoon1
-              </a>
-          </div>
-
-          <div className="hidden md:flex space-x-8 text-gray-600 font-medium">
-            <a href="#" className="hover:text-mint-500">who?</a>
-            <a href="#" className="hover:text-abigail-500">about</a>
-            <a href="#">contact</a>
-          </div>
-          <div className ="flex items-center space-x-4">
-            ASDFASDF
-          </div>
-        </nav>
-      </header>
-    </div>
+    <ThemeProvider>
+      <div className='App bg-white dark:bg-gray-900 min-h-screen transition-colors'>
+        <Header />
+        <Routes>  
+          <Route path="/" element={<div>Home Page</div>} />
+        </Routes>
+      </div>
+    </ThemeProvider>
   )
 }
 
