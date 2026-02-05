943c13fecd9d48bc51d203805094cc890875f302
comppy <kim1122aass@gmail.com>
2026-02-05 19:05:10 +0900

    setting/tailwind css 색상 커스텀

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
-import { useState } from 'react'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className='App'>
-      <button onClick={()=> setCount(count + 1)} className="text-4xl text-red-500 hover:text-green-700 ">count is {count}</button>
+      <header className='bg-white shadow-md'>
+        <nav className="container mx-auto px-6 py-4 flex justify-between items-center">
+          <div className="flex justify-between items-center"> 
+              <a href="/" className="text-2xl font-bold bg-gradient-to-r from-blue-600 to-indigo-600 bg-clip-text text-transparent">
+                seojoon1
+              </a>
+          </div>
+
+          <div className="hidden md:flex space-x-8 text-gray-600 font-medium">
+            <a href="#" className="hover:text-mint-500">who?</a>
+            <a href="#" className="hover:text-abigail-500">about</a>
+            <a href="#">contact</a>
+          </div>
+          <div className ="flex items-center space-x-4">
+            ASDFASDF
+          </div>
+        </nav>
+      </header>
     </div>
   )
 }
