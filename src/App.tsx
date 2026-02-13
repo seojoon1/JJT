daef14b245ced5500d2b990dbb342eef698dbb88
comppy <kim1122aass@gmail.com>
2026-02-13 15:43:30 +0900

    feat/scrolltotop

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { ThemeProvider } from "./contexts/ThemeContext"
 import { Routes, Route } from "react-router";
 import Home from "./pages/Home";
 import ProjectsPage from "./pages/ProjectsPage";
+import ScrollToTop from "./utiles/stt";
 
 function App() {
 
   return (
     <ThemeProvider>
+      <ScrollToTop />
       <div className='App bg-white dark:bg-gray-900 min-h-screen transition-colors'>
         <Header />
         <Routes>  
